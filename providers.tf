1f10036ac7c5449b503b5179aa68bbebfcfffbe8
Benelab <78307945+benesnpl@users.noreply.github.com>
2022-06-23 13:35:24 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -10,5 +10,9 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
+    features {}
+    subscription_id = "643419c7-11b4-43c2-9d6a-ee115f86627f"
+    client_id = "5dbdd145-9e11-4350-84aa-ef37502bb6cc"
+    client_secret = "fFT8Q~llpjDUJlNtuhMZ9zriZ.reit9NwED4cdtw"
+    tenant_id = "cf668d98-df58-45c9-86fb-b72c2761364b"
 }
